11ee8e7aa0b9624067da6f6fe953914b9d1f5e29
belloaliyu11 <308107046+belloaliyu11@users.noreply.github.com>
2026-08-28 12:15:03 +0100

    fix deploy workflow package manager setup

--- src/contracts/util.ts
+++ src/contracts/util.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { WalletNetwork } from "@creit.tech/stellar-wallets-kit";
+import { Networks as WalletNetwork } from "@creit.tech/stellar-wallets-kit";
 import { Network, NetworkType } from "../debug/types/types";
 
 const envSchema = z.object({
